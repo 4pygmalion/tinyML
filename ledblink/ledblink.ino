a9178cbfce01bf81311cd60714c9c37d17f8dfc0
4pygmalion <4pygmalion@naver.com>
2024-02-24 14:51:43 +0900

    ledblink complete

---
+++ ledblink/ledblink.ino
@@ -0,0 +1,14 @@
+
+// constatnt variable
+const int ledPin = 25;
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+}; 
+
+void loop() {
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+}
